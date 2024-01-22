a1e5d5fae27a293bec52ac3485821853cbf54f28
Niko Uusitalo <983351+nikouu@users.noreply.github.com>
2024-01-22 19:40:19 +1300

    Finished MD5 investigation
    MD5 for these cases is faster than SHA1

--- StringToGuidBenchmarking/Program.cs
+++ StringToGuidBenchmarking/Program.cs
@@ -2,10 +2,12 @@
 using BenchmarkDotNet.Running;
 using StringToGuidBenchmarking;
 
-var j = new Benchmarks.BenchmarkString("ABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDAB");
+var j = new Benchmarks.BenchmarkString("ABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDABABCDABCDAB");
 var g = new Benchmarks();
 
-//var result1 = g.Immo_SHA1_MemoryMarshal2(j);
-//var result2 = g.Immo_SHA1_MemoryMarshal(new Benchmarks.BenchmarkString("ASDF"));
+
+
+var result1 = g.Immo_Speed_Optimized(j);
+var result2 = g.Immo_Original(j);
 
 var summary = BenchmarkRunner.Run<Benchmarks>();
